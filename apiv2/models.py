5cfca3437652b7c806b2b5489d432d42396daedf
scotty-b <businge.scott@andela.com>
2017-07-24 14:26:29 +0300

    models for bucketlists and items

--- apiv2/models.py
+++ apiv2/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Bucketlist(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    bucketlist = models.ForeignKey(Bucketlist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
