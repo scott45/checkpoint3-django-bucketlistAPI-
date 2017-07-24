0888ca0482e27f0ebc92785ca5d97c7d84f7397b
scotty-b <businge.scott@andela.com>
2017-07-24 15:57:50 +0300

    serialise models

---
+++ apiv2/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Bucketlist, Item
+
+
+class BucketlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bucketlist
+        fields = ('name', 'date_created_at', 'date_modified', 'created_by')
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('name', 'date_created_at', 'date_modified', 'bucketlist')
