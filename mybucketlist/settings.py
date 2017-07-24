93a787bcb0786089da92fae7ed5c01b825a74c67
scotty-b <businge.scott@andela.com>
2017-07-24 17:32:52 +0300

    comment out rest_framework settings to bypass login

--- mybucketlist/settings.py
+++ mybucketlist/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# REST Framework
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [],
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
-}
+# # REST Framework
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [],
+#     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+# }
