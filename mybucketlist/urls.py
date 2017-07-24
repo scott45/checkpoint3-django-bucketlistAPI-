87e400758b44007df3daa89bc7d0709fba327dea
scotty-b <businge.scott@andela.com>
2017-07-24 17:33:13 +0300

    urls refactoring

--- mybucketlist/urls.py
+++ mybucketlist/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from ..apiv2 import views
+from apiv2 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(
-        r'^api/v1/puppies/(?P<pk>[0-9]+)$', views.get_delete_update_bucketlist,
-        name='get_delete_update_puppy'),
-    url(r'^api/v1/puppies/$', views.get_post_bucketlist, name='get_post_puppies')
+        r'^api/v1/bucketlists/(?P<pk>[0-9]+)$', views.get_delete_update_bucketlist,
+        name='get_delete_update_bucketlist'),
+    url(r'^api/v1/bucketlists/$', views.get_post_bucketlist, name='get_post_bucketlists')
 ]
