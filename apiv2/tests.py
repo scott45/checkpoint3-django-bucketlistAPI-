8fe23e79d9a261a3ca8943f955f693df5683f128
scotty-b <businge.scott@andela.com>
2017-07-24 15:47:39 +0300

    complete test

--- apiv2/tests.py
+++ apiv2/tests.py
@@ -2,9 +2,28 @@ from django.test import TestCase
 from .models import Bucketlist, Item
 
 
+# tests for bucketlist and item models
+
+
 class BucketlistTest(TestCase):
-    pass
+    def setUp(self):
+        Bucketlist.objects.create(name='Travel')
+        Bucketlist.objects.create(name='Ministry')
+
+    def test_result(self):
+        first = Bucketlist.objects.get(name='Travel')
+        second = Bucketlist.objects.get(name='Ministry')
+        self.assertEqual(first.get_name(), "Travel")
+        self.assertEqual(second.get_name(), "Ministry")
 
 
 class ItemTest(TestCase):
-    pass
+    def setUp(self):
+        Item.objects.create(name='Europe')
+        Item.objects.create(name='Souls')
+
+    def test_output(self):
+        firstitem = Bucketlist.objects.get(name='Europe')
+        seconditem = Bucketlist.objects.get(name='Souls')
+        self.assertEqual(firstitem.get_name(), "Europe")
+        self.assertEqual(seconditem.get_name(), "Souls")
