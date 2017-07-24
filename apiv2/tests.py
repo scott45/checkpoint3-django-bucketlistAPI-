ced69dfaa74cd3e962fe5d4c7a3a2becd6282c51
scotty-b <businge.scott@andela.com>
2017-07-24 17:32:00 +0300

    test views

--- apiv2/tests.py
+++ apiv2/tests.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
 from .models import Bucketlist, Item
+from .serializers import BucketlistSerializer, ItemSerializer
 
+
+# initialize the APIClient app
+client = Client()
 
 # tests for bucketlist and item models
 
@@ -27,3 +35,22 @@ class ItemTest(TestCase):
         seconditem = Bucketlist.objects.get(name='Souls')
         self.assertEqual(firstitem.get_name(), "Europe")
         self.assertEqual(seconditem.get_name(), "Souls")
+
+
+class GetAllPuppiesTest(TestCase):
+    """ Test module for GET all bucketlist API """
+
+    def setUp(self):
+        Bucketlist.objects.create(name='Travel')
+        Bucketlist.objects.create(name='Ministry')
+        Bucketlist.objects.create(name='Travel')
+        Bucketlist.objects.create(name='Ministry')
+
+    def test_get_all_bucketlists(self):
+        # get API response
+        response = client.get(reverse('get_post_bucketlists'))
+        # get data from db
+        blists = Bucketlist.objects.all()
+        serializer = BucketlistSerializer(blists, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
