1f14cf12db9a6e76b8eb274bd9780bc992fdedd2
scotty-b <businge.scott@andela.com>
2017-07-24 14:27:17 +0300

    configure postgres db settings

--- mybucketlist/settings.py
+++ mybucketlist/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'mybucketlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apiv2',
+        'USER': '<scotty>',
+        'PASSWORD': '<scottyapi>',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
