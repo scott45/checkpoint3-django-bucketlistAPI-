aba880676a6646535f6df04f12e2b2fc4211309b
scotty-b <businge.scott@andela.com>
2017-07-26 10:22:54 +0300

    refactor column to solve error

--- apiv2/serializers.py
+++ apiv2/serializers.py
@@ -5,10 +5,10 @@ from .models import Bucketlist, Item
 class BucketlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bucketlist
-        fields = ('name', 'date_created_at', 'date_modified', 'created_by')
+        fields = ('name', 'date_created', 'date_modified', 'created_by')
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ('name', 'date_created_at', 'date_modified', 'bucketlist')
+        fields = ('name', 'date_created', 'date_modified', 'bucketlist')
